8c7d8e469ddedfc4ddfd5f3a20f568d9fc4182cf
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-11 15:00:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(arr, event) {
+  const messages = [];
+  for (let i = 0; i < arr.length; i++) {
+    messages.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(i) {
+  let count = 0;
+  while (i >= count) {
+    console.log(i);
+    i--;
+  }
+}
